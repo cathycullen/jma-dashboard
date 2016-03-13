0463d3cc3066dcbf7683e53cb700dd5fae17060e
chcculle <cathy@softwareoptions.com>
2016-03-13 03:21:17 -0500

    add pg bootstrap coaches

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+coach = Coach.create(name: "Jody Michael", email: "jody@jodymichael.com", phone: "312-307-3031")
+coach = Coach.create(name: "Maura Koutoujian", email: "maura@jodymichael.com", phone: "312-218-0016")
+coach = Coach.create(name: "Nancy Scheel", email: "nancy@jodymichael.com", phone: "786-553-4644")
+
+
